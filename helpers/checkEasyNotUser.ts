69c963eda81f715ff5f98a827d1ed31e24e93158
Md. Sabbir Hossain <shr472978@gmail.com>
2025-02-03 11:30:41 +0600

    maybe

--- helpers/checkEasyNotUser.ts
+++
@@ -1,9 +0,0 @@
-export const checkEasyNotUser = (
-    store: any,
-    easyOrderValue: any,
-    defaultValue: any,
-    isAuthenticated: boolean
-) =>
-    store?.auth_type === 'EasyOrder' && !isAuthenticated
-        ? easyOrderValue
-        : defaultValue;
--- helpers/numberParser.ts
+++
@@ -1,8 +0,0 @@
-
-export const numberParser = (digit: any,ceil?:boolean): number => {
-    const parsed = parseFloat(digit);
-    if(ceil){
-        return isNaN(parsed) ? 0 : Math.ceil(parseFloat(parsed.toFixed(2)));
-    }
-    return isNaN(parsed) ? 0 : parseFloat(parsed.toFixed(2));
-};
