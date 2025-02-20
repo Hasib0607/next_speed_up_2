0896faf0860125cb23424f45fb1cddde36c0dc3d
Md. Sabbir Hossain <shr472978@gmail.com>
2025-02-20 12:19:09 +0600

    rate update

--- utils/rate.tsx
+++ utils/rate.tsx
@@ -1,25 +1,24 @@
-'use client'
-import { RootState } from '@/redux/store';
+'use client';
+
 import StarRatings from 'react-star-ratings';
-import { useAppSelector } from '@/redux/features/rtkHooks/rtkHooks';
+import { useGetHeaderSettingsQuery } from '@/redux/features/home/homeApi';
 
 const Rate = ({ rating, className }: any) => {
-    const store_id = useAppSelector(
-        (store: RootState) => store.products.storeId
-    );
+    const { data: headerSettingData } = useGetHeaderSettingsQuery({});
+    const { store_id } = headerSettingData?.data || {};
 
-    return (
-        store_id !== 10218 && (
-            <div className={className}>
-                <StarRatings
-                    rating={rating}
-                    starRatedColor="#FBC029"
-                    numberOfStars={5}
-                    starDimension="16px"
-                    starSpacing="2px"
-                />
-            </div>
-        )
+    return store_id && parseInt(store_id) === 10218 ? (
+        <div></div>
+    ) : (
+        <div className={className}>
+            <StarRatings
+                rating={rating}
+                starRatedColor="#FBC029"
+                numberOfStars={5}
+                starDimension="16px"
+                starSpacing="2px"
+            />
+        </div>
     );
 };
 
