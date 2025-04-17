02835f0128d607c6fcd058e0d159fca97a611485
Ashikuzzaman Moon <ashikuzzamanmoon31@gmail.com>
2025-04-17 16:49:05 +0600

    landing products

--- utils/dynamic-import/shops/shops.ts
+++ utils/dynamic-import/shops/shops.ts
@@ -1,4 +1,4 @@
-import { FORTY_THREE } from '@/consts';
+import { FORTY_FOUR, FORTY_THREE } from '@/consts';
 import dynamic from 'next/dynamic';
 
 export const shops: any = {
@@ -56,9 +56,6 @@ export const shops: any = {
     twentyeight: dynamic(
       () => import("@/components/_shop-page/twenty-eight/twenty-eight")
     ),
-    [FORTY_THREE]: dynamic(
-      () => import("@/components/_shop-page/forty-three/forty-three")
-    ),
     // twentynine: dynamic(
     //   () => import("@/components/headers/header-twentynine/header-twentynine")
     // ),
@@ -92,4 +89,10 @@ export const shops: any = {
     // forty: dynamic(
     //   () => import("@/components/headers/header-forty/header-forty")
     // ),
+    [FORTY_THREE]: dynamic(
+        () => import("@/components/_shop-page/forty-three/forty-three")
+      ),
+    [FORTY_FOUR]: dynamic(
+        () => import("@/components/_shop-page/forty-four/forty-four")
+      ),
 };
