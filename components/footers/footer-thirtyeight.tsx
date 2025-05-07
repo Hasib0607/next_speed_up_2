0819e852a76be5fbbd45f2076d2177f9d9debe1f
Ashikuzzaman Moon <ashikuzzamanmoon31@gmail.com>
2025-05-07 12:13:51 +0600

    footer

--- components/footers/footer-thirtyeight.tsx
+++ components/footers/footer-thirtyeight.tsx
@@ -61,7 +61,7 @@ const FooterThirtyEight = ({
                     </div>
 
                     <div className="xl:col-span-2 lg:col-span-2">
-                        <div className="grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-4 px-5">
+                        <div className="grid xl:grid-cols-5 lg:grid-cols-5 md:grid-cols-2 sm:grid-cols-1 gap-4 px-5">
                             <div>
                                 <h1 className="text-sm tracking-widest uppercase">
                                     Products
@@ -81,22 +81,36 @@ const FooterThirtyEight = ({
                             </div>
                             <div>
                                 <h1 className="text-sm tracking-widest uppercase">
-                                    Pages
+                                    Menu
                                 </h1>
                                 <div className="mt-5 list-none">
-                                    {menu?.map((m: any) =>
-                                        m?.name !== 'Category' ? (
-                                            <li key={m?.id}>
-                                                <Link
-                                                    href={m?.url}
-                                                    className="text-base footerColor font-normal leading-relaxed text-[#666666]"
-                                                >
-                                                    {' '}
-                                                    {m?.name}
-                                                </Link>
-                                            </li>
-                                        ) : null
+                                    {menu?.map(
+                                        (m: any) =>
+                                            m?.name !== 'Category' &&
+                                            m.status == 1 && (
+                                                <li key={m?.id}>
+                                                    <Link
+                                                        href={
+                                                            m?.custom_link ||
+                                                            (m?.url
+                                                                ? `/${m?.url}`
+                                                                : '/')
+                                                        }
+                                                        className="text-base footerColor font-normal leading-relaxed text-[#666666]"
+                                                    >
+                                                        {' '}
+                                                        {m?.name}
+                                                    </Link>
+                                                </li>
+                                            )
                                     )}
+                                </div>
+                            </div>
+                            <div>
+                                <h1 className="text-sm tracking-widest uppercase">
+                                    Legal
+                                </h1>
+                                <div className="mt-5 list-none">
                                     {result?.map((m: any) => (
                                         <li key={m?.id}>
                                             <Link
